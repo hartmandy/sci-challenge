1d75416cb21c4b6543492ed2244d891ab9fd55fe
hartmandy <120344775+hartmandy@users.noreply.github.com>
2024-12-17 18:56:09 -0500

    types

---
+++ src/app/types.ts
@@ -0,0 +1,22 @@
+export type CardData = {
+  id: string; // NOTE: Added id
+  set: string;
+  number: string;
+  name: string;
+  type: string;
+  aspects: string[];
+  traits: string[];
+  arenas: string[];
+  cost: number;
+  power: number;
+  hp: string; // NOTE: changed to type string
+  fronttext: string;
+  doublesided: boolean;
+  rarity: string;
+  unique: boolean;
+  artist: string;
+  varianttype: string;
+  marketprice: string;
+  foilprice: string;
+  frontArt: string;
+};
