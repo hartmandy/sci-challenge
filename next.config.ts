58ca2d380cbb197cef6006a2a6b0903f952d1a3f
hartmandy <120344775+hartmandy@users.noreply.github.com>
2024-12-17 18:57:02 -0500

    image hostname

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // NOTE: Updated to match the pathname and hostname of their cdn.
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.swu-db.com",
+        port: "",
+        pathname: "/images/cards/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
