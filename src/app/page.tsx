4ad40efad75a0be49f85af2fe5cd74565ed46cbb
hartmandy <120344775+hartmandy@users.noreply.github.com>
2024-12-17 18:56:30 -0500

    page and layout updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
-'use client';
-import React, { useState } from 'react';
-import Dropdown from './components/Dropdown';
-import CardList from './components/CardList';
+"use client";
+import React, { useState } from "react";
+import CardList from "./components/CardList";
+import { ModeToggle } from "./components/ModeToggle";
+import CardFilters from "./components/CardFilters";
 
 export default function Page() {
-  const [selectedHp, setSelectedHp] = useState<string>('');
-
+  // NOTE: Added toggle for light and dark mode to page
   return (
-      <main>
-        <h1>Card Browser</h1>
-        <Dropdown onSelect={setSelectedHp} />
-        {selectedHp && <CardList hp={selectedHp} />}
-      </main>
+    <main>
+      <div className="sticky top-0 left-0 right-0 dark:bg-zinc-900 bg-zinc-100 border-b z-10">
+        <div className="container mx-auto flex justify-between p-4">
+          <CardFilters />
+          <ModeToggle />
+        </div>
+      </div>
+      <div className="p-4 container mx-auto">
+        <CardList />
+      </div>
+    </main>
   );
 }
